3cd030c66c324aea7ea921e021448de260a12823
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-07-04 20:37:38 -0300

    feat: configura ambientes e URL base da API

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { 
+    ApplicationConfig, 
+    provideBrowserGlobalErrorListeners
+} from '@angular/core';
+import { provideHttpClient } from '@angular/common/http'; 
 import { provideRouter } from '@angular/router';
 
+import { environment } from '../environments/environment.development';
 import { routes } from './app.routes';
+import { API_URL } from './core/config/api-url.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideHttpClient(),
+    provideRouter(routes), 
+    {
+        provide: API_URL,
+        useValue: environment.apiUrl,
+    },
+  ],
 };
